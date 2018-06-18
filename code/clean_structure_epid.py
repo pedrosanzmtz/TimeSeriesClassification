2188182d965f68703051b4c09ee882017300862d
pedrosanzmtz <pedrosanzmtz@gmail.com>
2018-06-18 06:45:58 -0500

    code to clean and structure virus data

---
+++ code/clean_structure_epid.py
@@ -0,0 +1,26 @@
+from sys import argv
+import pandas as pd 
+
+def week_to_station(week):
+	if week >= 51 or week <= 11:
+		return 'winter'
+	if week >= 12 and week <= 25:
+		return 'spring'
+	if week >= 26 and week <= 34:
+		return 'summer'
+	if week >= 35 and week <= 50:
+		return 'fall'
+
+def custom_aggregation(x):
+	m = x.mean()
+	return 1 if(m >= 0.5) else 0
+
+if __name__ == '__main__':
+	file_name = argv[1]
+	df = pd.read_csv(file_name)
+	df = df.drop(['Men', 'Women'], axis=1)
+	df['Station'] = df['Week'].apply(week_to_station)
+	df = df.drop(['Week'], axis=1)
+	dfGroupBy = df.groupby(['State', 'Year', 'Station']).agg(['sum'])
+	final = pd.concat([dfGroupBy['MenN'], dfGroupBy['WomenN']])
+	dfGroupBy.to_csv('new_' + argv[1], index=False)
